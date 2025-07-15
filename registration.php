cd61d7cd435dc095ff782e8dd3040dc4724588e2
MohanedEhab2003 <mohanedehab42@gmail.com>
2025-07-15 17:41:37 +0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+$data=$_POST;
+echo $data['fullname'];
+echo '<br>';
+echo $data['email'];
+echo '<br>';
+echo $data['password'];
+echo '<br>';
+echo $data['ConfirmPassword'];
+echo '<br>';
+echo $data['Gender'];
+echo '<br>';
+echo $data['Hobbies'];
+echo '<br>';
+echo $data['Country'];
+?>
